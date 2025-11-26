df5af27970f5aa59f7b9bf20cce60e2a65f3dbd5
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 13:44:48 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files --ignore=github.com/pjbgf/sha1cd
 
 # Manually copy licenses that go-licenses could not download
